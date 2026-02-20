f0f0bce41b0b747a299ee039841602fac0b20bb6
Zorynth Bendy <142782141+zorynith@users.noreply.github.com>
2026-02-20 21:44:28 +0800

    Add Safari pinned tab icon and theme color meta tags

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -23,6 +23,14 @@ const vitePressOptions = {
     ['link', { rel: 'apple-touch-icon', sizes: '144x144', href: '/resources/apple-icon-144x144.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/resources/apple-icon-152x152.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/resources/apple-icon-180x180.png' }],
+    
+    ['link', { rel: 'mask-icon', href: '/resources/safari-pinned-tab.svg', color: '#6ac969' }]
+    ['meta', { name: 'theme-color', content: '#c8f3e5', media: '(prefers-color-scheme: light)' }],
+    ['meta', { name: 'theme-color', content: '#33554b', media: '(prefers-color-scheme: dark)' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/resources/mstile-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3b675b' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/resources/favicon.svg' }],
+    ['link', { rel: 'preload', as: 'image', href: '/resources/favicon.ico' }],
 
     ['link', { rel: 'stylesheet', href: '/custom.css' }]
   ],
