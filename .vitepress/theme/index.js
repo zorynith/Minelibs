157bd916ff22c0f2e11db986476b5a1a5dd7e7ea
Zorynth Bendy <142782141+zorynith@users.noreply.github.com>
2026-03-17 16:19:10 +0800

    Refactor Layout to use defineComponent and setup

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,27 @@
-import { h } from 'vue'
+import { h, defineComponent } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
 import './style.css'
 import VersionSelector from './components/VersionSelector.vue'
-import VideoLayout from './components/VideoLayout.vue' // 导入自定义布局
+import VideoLayout from './components/VideoLayout.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {})
-  },
+  Layout: defineComponent({
+    setup() {
+      const { frontmatter } = useData()
+      return () => {
+        // 如果 frontmatter 中指定了 layout: VideoLayout，则使用自定义视频布局
+        if (frontmatter.value?.layout === 'VideoLayout') {
+          return h(VideoLayout)
+        }
+        // 否则使用默认主题布局
+        return h(DefaultTheme.Layout)
+      }
+    }
+  }),
   enhanceApp({ app }) {
-    // 注册全局组件
     app.component('VersionSelector', VersionSelector)
-    app.component('VideoLayout', VideoLayout) // 注册 VideoLayout
+    app.component('VideoLayout', VideoLayout)
   }
 }
